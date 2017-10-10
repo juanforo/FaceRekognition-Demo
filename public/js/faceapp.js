22a5b6cca311858a896ebb9ab584bcac994d00a2
Juan Ruiz <Juan.Ruiz@endava.com>
2017-10-10 06:34:00 -0500

    Implements several ID Cases and fallback case

--- public/js/faceapp.js
+++ public/js/faceapp.js
@@ -30,13 +30,18 @@ $(document).ready(function() {
 
             //If JUAN
 
-            if(data.id == "JUAN"){
+            if(data.id.toUpperCase() == "JUAN"){
                 $('#status').find('.texts li:first').text("Success!");
+                $('#name').find('.texts li:first').text("Name: Juan Sebastian Ruiz Villa");
+                $('#newTickets').find('.texts li:first').text("New Tickets: 3");
+                $('#ongoingTickets').find('.texts li:first').text("Ongoing Tickets: 1");
+                $('#projects').find('.texts li:first').text("New Project: Speedpay");
+                $('#alert1').find('.texts li:first').text("Alert: Oracle Timesheets!");
                 $('.tlt').textillate('in');
                 $('.tlt2').textillate('in');
                 intervalManager(false);
                 // create speech response
-                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have 3 new tickets, and 1, new project awaiting for you!, also, please remember to fill your oracle timesheets"}, function(response) {
+                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have 3 new tickets, 1, ongoing ticket, and you were included on the speedpay project, also, please remember to fill your oracle timesheets"}, function(response) {
                     $("#audio_speech").attr("src", "data:audio/mpeg;base64," + response);
                     $("#audio_speech")[0].play();
                 });
@@ -44,44 +49,60 @@ $(document).ready(function() {
 
 
             //if Hollman
-            else if(data.id == "HOLLMAN"){
+            else if(data.id.toUpperCase() == "HOLLMAN"){
                 $('#status').find('.texts li:first').text("Success!");
+                $('#name').find('.texts li:first').text("Name: Hollman Eduardo Enciso Peña");
+                $('#newTickets').find('.texts li:first').text("New Tickets: 0");
+                $('#ongoingTickets').find('.texts li:first').text("Ongoing Tickets: 1");
+                $('#projects').find('.texts li:first').text("New Project: Buzzpoints");
+                $('#alert1').find('.texts li:first').text("Alert: Oracle Timesheets!");
+                $('#alert2').find('.texts li:first').text("Alert: Travel Expenses!!");
                 $('.tlt').textillate('in');
+                $('.tlt2').textillate('in');
                 intervalManager(false);
                 // create speech response
-                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have 3 new tickets, and 1, new project awaiting for you!, also, please remember to fill your oracle timesheets"}, function(response) {
+                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have no new tickets, 2, ongoing tickets, and you look pretty danm gay. Also please remember to fill your oracle timesheets, and log your travel expenses"}, function(response) {
                     $("#audio_speech").attr("src", "data:audio/mpeg;base64," + response);
                     $("#audio_speech")[0].play();
                 });
             }
 
             //if Julian
-            else if(data.id == "Julian"){
+            else if(data.id.toUpperCase() == "JULIAN"){
                 $('#status').find('.texts li:first').text("Success!");
+                $('#name').find('.texts li:first').text("Name: Julian Forero");
+                $('#newTickets').find('.texts li:first').text("New Tickets: 4");
+                $('#ongoingTickets').find('.texts li:first').text("Ongoing Tickets: 1");
+                $('#projects').find('.texts li:first').text("Current Project: Mclatchy");
                 $('.tlt').textillate('in');
+                $('.tlt2').textillate('in');
                 intervalManager(false);
                 // create speech response
-                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have 3 new tickets, and 1, new project awaiting for you!, also, please remember to fill your oracle timesheets"}, function(response) {
+                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have 4 new tickets, and 1, ongoing ticket. There are no changes regarding your project allocation"}, function(response) {
                     $("#audio_speech").attr("src", "data:audio/mpeg;base64," + response);
                     $("#audio_speech")[0].play();
                 });
-            }
+            }  //if someone else
 
             else {
 
                 $('#status').find('.texts li:first').text("Success!");
+                $('#name').find('.texts li:first').text("Name: " + data.id );
+                $('#newTickets').find('.texts li:first').text("Tickets Information Unretrievable");
+                $('#projects').find('.texts li:first').text("Project Allocation Unretrievable");
                 $('.tlt').textillate('in');
+                $('.tlt2').textillate('in');
                 intervalManager(false);
                 // create speech response
-                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Today you have 3 new tickets, and 1, new project awaiting for you!, also, please remember to fill your oracle timesheets"}, function(response) {
+                $.post("/speech", {tosay: "Good " + greetingTime(moment()) + " " + data.id + ". Welcome to eendava. Sorry, I cannot retrieve your full profile your data, please contact the system administrator"}, function(response) {
                     $("#audio_speech").attr("src", "data:audio/mpeg;base64," + response);
                     $("#audio_speech")[0].play();
                 });
             }
 
 
-
-            setTimeout(function () {intervalManager(true); $('.tlt').textillate('out');},10000);
+            //starts pause period
+            setTimeout(function () {intervalManager(true); $('.tlt').textillate('out'); $('.tlt2').textillate('out');},10000);
 
             $('#status').find('.texts li:first').text("Identifying...");
 
@@ -234,6 +255,11 @@ $(document).ready(function() {
     // Define what the button clicks do.
     //starts timer
     intervalManager(true);
+
+    $.post("/speech", {tosay: "Hello! I am the Luciana project"}, function(response) {
+        $("#audio_speech").attr("src", "data:audio/mpeg;base64," + response);
+        $("#audio_speech")[0].play();
+    });
     // Initiate the camera widget on screen
     var options = {
       swf_url: "js/jpeg_camera/jpeg_camera.swf",
@@ -242,7 +268,6 @@ $(document).ready(function() {
 
 
     camera = new JpegCamera("#camera", options).ready(function(info) {
-      $("#loading_img").hide();
     });
 
   }
